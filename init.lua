9dad20851f5235087b2136a475847834971528ed
李槟 <1961774471@qq.com>
2023-07-29 23:18:54 +0800

    添加vscode的配置

--- init.lua
+++ init.lua
@@ -1,37 +1,38 @@
-require("lee.plugins-setup")
+if vim.g.vscode then
+  -- VSCode extension
+else
+  require("lee.plugins-setup")
 
--- 核心配置
-require("lee.core.helpers")
-require("lee.core.options")
-require("lee.core.keymaps")
-require("lee.core.colorscheme")
+  -- 核心配置
+  require("lee.core.helpers")
+  require("lee.core.options")
+  require("lee.core.keymaps")
+  require("lee.core.colorscheme")
 
--- 工具
-require("lee.utils.im-select")
+  -- 工具
+  require("lee.utils.im-select")
 
--- 插件配置
-require("lee.plugins.comment")
-require("lee.plugins.nvim-tree")
-require("lee.plugins.lualine")
-require("lee.plugins.telescope")
-require("lee.plugins.nvim-cmp")
-require("lee.plugins.lsp.mason")
-require("lee.plugins.lsp.lspsaga")
-require("lee.plugins.lsp.lspconfig")
-require("lee.plugins.lsp.null-ls")
-require("lee.plugins.autopairs")
-require("lee.plugins.treesitter")
-require("lee.plugins.gitsigns")
-require("lee.plugins.toggleterm")
-require("lee.plugins.copilot")
--- require("lee.plugins.cleverf")
-require("lee.plugins.comment")
-require("lee.plugins.bufferline")
-require("lee.plugins.indent-blankline")
-require("lee.plugins.accelerated-jk")
-require("lee.plugins.colorizer")
-require("lee.plugins.hop")
--- require("lee.plugins.tabout")
-
--- 打印信息
-print("Lee's NeoVim config loaded!")
+  -- 插件配置
+  require("lee.plugins.comment")
+  require("lee.plugins.nvim-tree")
+  require("lee.plugins.lualine")
+  require("lee.plugins.telescope")
+  require("lee.plugins.nvim-cmp")
+  require("lee.plugins.lsp.mason")
+  require("lee.plugins.lsp.lspsaga")
+  require("lee.plugins.lsp.lspconfig")
+  require("lee.plugins.lsp.null-ls")
+  require("lee.plugins.autopairs")
+  require("lee.plugins.treesitter")
+  require("lee.plugins.gitsigns")
+  require("lee.plugins.toggleterm")
+  require("lee.plugins.copilot")
+  -- require("lee.plugins.cleverf")
+  require("lee.plugins.comment")
+  require("lee.plugins.bufferline")
+  require("lee.plugins.indent-blankline")
+  require("lee.plugins.accelerated-jk")
+  require("lee.plugins.colorizer")
+  require("lee.plugins.hop")
+  -- require("lee.plugins.tabout")
+end
