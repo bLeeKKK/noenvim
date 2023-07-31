86fdc0286f9985375fbf6385192c0b80dfd0d439
李槟 <1961774471@qq.com>
2023-07-31 09:52:54 +0800

    添加快捷键提示

--- lua/lee/plugins-setup.lua
+++ lua/lee/plugins-setup.lua
@@ -209,6 +209,19 @@ return packer.startup(function(use)
     after = { "nvim-cmp" }, -- if a completion plugin is using tabs load it before
   })
 
+  use({
+    "folke/which-key.nvim",
+    config = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+      require("which-key").setup({
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      })
+    end,
+  })
+
   if packer_bootstrap then
     require("packer").sync()
   end
