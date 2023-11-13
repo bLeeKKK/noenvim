038d7c5271ad82d0038d4dfa04c2c3a90fe39fd7
bin6.li <bin6.li@changhong.com>
2023-11-13 09:44:39 +0800

    处理更新

--- lua/lee/plugins/indent-blankline.lua
+++ lua/lee/plugins/indent-blankline.lua
@@ -1,57 +1,6 @@
-local status, blankline = pcall(require, "indent_blankline")
+local status, ibl = pcall(require, "ibl")
 if not status then
   return
 end
 
--- 第一种
--- vim.opt.termguicolors = true
--- vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
--- vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
--- vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
--- vim.cmd([[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]])
--- vim.cmd([[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]])
--- vim.cmd([[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]])
---
--- vim.opt.list = true
--- vim.opt.listchars:append("space:⋅")
--- vim.opt.listchars:append("eol:↴")
--- blankline.setup({
---   space_char_blankline = " ",
---   char_highlight_list = {
---     "IndentBlanklineIndent1",
---     "IndentBlanklineIndent2",
---     "IndentBlanklineIndent3",
---     "IndentBlanklineIndent4",
---     "IndentBlanklineIndent5",
---     "IndentBlanklineIndent6",
---   },
--- })
-
--- 第二种
-vim.opt.list = true
-vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
-
-blankline.setup({
-  space_char_blankline = " ",
-  show_current_context = true,
-  show_current_context_start = true,
-})
-
--- 第三种
--- vim.opt.termguicolors = true
--- vim.cmd([[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]])
--- vim.cmd([[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]])
---
--- blankline.setup({
---   char = "",
---   char_highlight_list = {
---     "IndentBlanklineIndent1",
---     "IndentBlanklineIndent2",
---   },
---   space_char_highlight_list = {
---     "IndentBlanklineIndent1",
---     "IndentBlanklineIndent2",
---   },
---   show_trailing_blankline_indent = false,
--- })
+ibl.setup()
