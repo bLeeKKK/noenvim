d77087a7c94498bd39e40ffe40ee08693b9e29e0
bin6.li <bin6.li@changhong.com>
2024-05-07 13:38:39 +0800

    修改Copilot配置为18版本

--- lua/lee/plugins/copilot.lua
+++ lua/lee/plugins/copilot.lua
@@ -1,6 +1,9 @@
 require("copilot").setup({
   suggestion = { enabled = false },
   panel = { enabled = false },
+
+  -- 配置node版本
+  copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v18.18.1/bin/node", -- Node.js version must be > 18.x
 })
 
 require("CopilotChat").setup({
